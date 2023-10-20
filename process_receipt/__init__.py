d99de262617eb720b71ab418a95493d4f42fa561
Andres <95327624+scarfon@users.noreply.github.com>
2023-10-19 22:27:20 -0300

    optimized code

--- process_receipt/__init__.py
+++ process_receipt/__init__.py
@@ -26,6 +26,8 @@ class ReceiptEncoder(json.JSONEncoder):
         else:
             return super().default(obj)
 
+# TODO: REFACTOR CNPJ CODE TO A FUNCTION
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -43,34 +45,49 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         response = {}
         urllib.parse.unquote(imgUrl)
         # Get image from URL
+        response['id_ocr'] = str(uuid.uuid4())
 
         img_data = requests.get(imgUrl).content
         nparr = np.frombuffer(img_data, np.uint8)
-        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        original_image = image.copy()
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
         # Preprocess image
         try:
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            thresh = cv2.adaptiveThreshold(
-                gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 101, 1)
-            contours, hierarchy = cv2.findContours(
-                thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            largest_contour = max(contours, key=cv2.contourArea)
-            x, y, w, h = cv2.boundingRect(largest_contour)
-            x1 = x
-            y1 = y
-            x2 = x + w
-            y2 = y + h
-            cropped_image = original_image[y1:y2, x1:x2]
-            kernel = np.array([[1, 0, 0], [0, 1.8, 0], [0, 0, -1]])
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            thresh3 = cv2.threshold(gray, 155, 255, cv2.THRESH_BINARY)[1]
+
+            kernel = np.ones((7, 7), np.uint8)
+            morph = cv2.morphologyEx(thresh3, cv2.MORPH_CLOSE, kernel)
+            kernel = np.ones((9, 9), np.uint8)
+            morph = cv2.morphologyEx(morph, cv2.MORPH_ERODE, kernel)
+
+            contours = cv2.findContours(
+                morph, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            contours = contours[0] if len(contours) == 2 else contours[1]
+            area_thresh = 0
+            for c in contours:
+                area = cv2.contourArea(c)
+                if area > area_thresh:
+                    area_thresh = area
+                    big_contour = c
+
+            x, y, w, h = cv2.boundingRect(big_contour)
+
+            third_image = img[y:y+h, x:x+w]
+            height = third_image.shape[0]
+            width = third_image.shape[1]
+            third_image = third_image[0:int(height/3), 0:width]
+
+            cropped_image = img[y:y+h, x:x+w]
+            kernel = np.array(
+                [[1, 0, 0], [0, 1.9, 0], [0, 0, -1]], dtype=np.float32)
             cropped_image = cv2.filter2D(cropped_image, -1, kernel)
             cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
-            cropped_image = cv2.equalizeHist(cropped_image)
-            cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_GRAY2BGR)
+            cropped_image = cv2.adaptiveThreshold(
+                cropped_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 101, 1)
         except Exception as e:
             response['error'] = str(e)
-            cropped_image = original_image
+            cropped_image = img
 
         # Call Document AI Receipt API
         try:
@@ -83,10 +100,13 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             poller = document_analysis_client.begin_analyze_document(
                 "prebuilt-receipt", document=image_bytes)
             receipts = poller.result()
+
+            image_third_bytes = cv2.imencode('.jpg', third_image)[1].tobytes()
+            poller2 = document_analysis_client.begin_analyze_document(
+                'prebuilt-document', document=image_third_bytes)
+            key_value = poller2.result()
         except Exception as e:
             return func.HttpResponse(f"{str(e)}", status_code=400)
-
-        response['id_ocr'] = str(uuid.uuid4())
 
         try:
             connect_str = os.environ["AZURE_STORAGE_CONNECTION_STRING"]
@@ -104,41 +124,76 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             response['error'] = str(e)
 
         try:
-            pattern_cnpj = r"(CNPJ)(.?.?.?)\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2}"
-            pattern_cnpj_so = r"\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2}"
-            pattern_moeda = r"((.?.?.?)|R?)\$"
+            for kv in key_value.key_value_pairs:
+                if "CNPJ" in kv.key.content.upper():
+                    cnpj_kv = kv.value.content
+                    response['cnpj'] = cnpj_kv
+                    cnpj_kv = cnpj_kv.replace('.', '').replace(
+                        '/', '').replace('-', '')
+                    print(cnpj_kv + "kv before if")
+                    if re.search(r'\d{9}', cnpj_kv):
+                        print(cnpj_kv + "kv after if")
+                        cnpj_re = requests.get(
+                            'https://publica.cnpj.ws/cnpj/' + cnpj_kv)
+                        if cnpj_re.status_code == 200:
+                            cnpj = cnpj_re.json()
+                            response['cidade'] = cnpj['estabelecimento']['cidade']['nome'] if 'cidade' in cnpj['estabelecimento'] else ''
+                            divisao = cnpj['estabelecimento']['atividade_principal'][
+                                'divisao'] if 'atividade_principal' in cnpj['estabelecimento'] else ''
+                            if divisao != '':
+                                tipo_re = requests.get(
+                                    'https://servicodados.ibge.gov.br/api/v2/cnae/divisoes/' + divisao)
+                                if tipo_re.status_code == 200:
+                                    tipo = tipo_re.json()
+                                    response['tipo'] = tipo['descricao']
+                                if tipo_re.status_code != 200:
+                                    response['tipo'] = ''
+                        if cnpj_re.status_code != 200:
+                            response['cnpj'] = ''
+                            response['cidade'] = ''
+                            response['tipo'] = ''
+                    else:
+                        cnpj_kv = ''
+            if not cnpj_kv:
+                print("KV not found")
+                pattern_cnpj = r"(CNPJ)(.*)?\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2}"
+                pattern_cnpj_so = r"\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2}"
+                cnpj = re.search(pattern_cnpj, receipts.content, re.IGNORECASE)
+                if cnpj:
+                    cnpj = cnpj.group()
+                    cnpj = re.search(pattern_cnpj_so, cnpj)
+                    response['cnpj'] = cnpj.group()
+                    cnpj = response["cnpj"].replace(
+                        '.', '').replace('/', '').replace('-', '')
+                    cnpj_re = requests.get(
+                        'https://publica.cnpj.ws/cnpj/' + cnpj)
+                    if cnpj_re.status_code == 200:
+                        cnpj = cnpj_re.json()
+                        response['cidade'] = cnpj['estabelecimento']['cidade']['nome'] if 'cidade' in cnpj['estabelecimento'] else ''
+                        divisao = cnpj['estabelecimento']['atividade_principal']['divisao']
+                        tipo_re = requests.get(
+                            'https://servicodados.ibge.gov.br/api/v2/cnae/divisoes/' + divisao)
+                        if tipo_re.status_code == 200:
+                            tipo = tipo_re.json()
+                            response['tipo'] = tipo['descricao']
+                        if tipo_re.status_code != 200:
+                            response['tipo'] = ''
+                    if cnpj_re.status_code != 200:
+                        response['cnpj'] = ''
+                        response['cidade'] = ''
+                        response['tipo'] = ''
+                if not cnpj:
+                    response['cnpj'] = ''
+                    response['cidade'] = ''
+                    response['tipo'] = ''
+
+            pattern_moeda = r"(R?)\$"
             pattern_tipo_pag = r"(Cr(é|e)dito|D(é|e)bito|Dinheiro)"
             tipo_pag = re.search(
                 pattern_tipo_pag, receipts.content, re.IGNORECASE)
             response['tipo_pagamento'] = tipo_pag.group() if tipo_pag else ''
             moeda = re.search(pattern_moeda, receipts.content)
             response['moeda'] = moeda.group() if moeda else 'R$'
-            cnpj = re.search(pattern_cnpj, receipts.content, re.IGNORECASE)
-            if cnpj:
-                cnpj = cnpj.group()
-                cnpj = re.search(pattern_cnpj_so, cnpj)
-                response['cnpj'] = cnpj.group()
-                cnpj = response["cnpj"].replace(
-                    '.', '').replace('/', '').replace('-', '')
-                cnpj_re = requests.get(
-                    os.environ["API_ENDPOINT_CNPJ"] + cnpj)
-                if cnpj_re.status_code == 200:
-                    cnpj = cnpj_re.json()
-                    response['cidade'] = cnpj['estabelecimento']['cidade']['nome']
-                    divisao = cnpj['estabelecimento']['atividade_principal']['divisao']
-                    tipo_re = requests.get(
-                        os.environ["API_ENDPOINT_CNAE"] + divisao)
-                    if tipo_re.status_code == 200:
-                        tipo_re = tipo_re.json()
-                        response['tipo'] = tipo_re['descricao']
-                    if tipo_re.status_code != 200:
-                        response['tip'] = ''
-                if cnpj_re.status_code != 200:
-                    response['error'] = 'CNPJ não encontrado'
-            if not cnpj:
-                response['cnpj'] = ''
-                response['cidade'] = ''
-                response['tipo'] = ''
         except Exception as e:
             response['error'] = str(e)
 
